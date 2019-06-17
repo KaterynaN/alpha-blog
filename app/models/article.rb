1d902e0120b7c96a9e864ccffa3ce7ebbb63bfe6
Kateryna <kateryna.od.ua@gmail.com>
2019-06-17 13:48:16 +0300

    Add assotiation with user and article, add has_secure_password for user authntication

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ApplicationRecord
-  validates :title, presence: true, length: {minimum: 3, maximum: 60}
-  validates :description, presence: true, length: {minimum: 10, maximum: 300}
+  validates :title, presence: true,
+                    length: {minimum: 3, maximum: 60}
+  validates :description, presence: true,
+                          length: {minimum: 10, maximum: 300}
+
+  belongs_to :user
 end
