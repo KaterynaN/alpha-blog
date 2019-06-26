5d9c227c9c927a7c315d958017ba78b8fcdbd096
Kateryna <kateryna.od.ua@gmail.com>
2019-06-27 00:24:44 +0300

    Add methods for autorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+
+before_action :set_user, only: [:show, :edit, :update]
+
   def new
     @user = User.new
   end
@@ -14,11 +17,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-      @user = User.find(params[:id])
       if @user.update(user_params)
         flash[:success] = "Your accout was successfully updated"
         redirect_to articles_path
@@ -28,7 +30,6 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 3)
   end
 
@@ -40,6 +41,10 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
